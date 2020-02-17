694fb56d732d21f56c330dfa5f9838c004fdc719
Okko Hakola <ohakola@c2r6p4.hive.fi>
2020-02-17 18:34:12 +0200

    Add comment

--- src/fractals/magnet.c
+++ src/fractals/magnet.c
@@ -6,8 +6,9 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/17 17:51:06 by ohakola           #+#    #+#             */
-/*   Updated: 2020/02/17 17:51:16 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/02/17 18:33:53 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // https://fractalfoundation.org/OFC/OFC-5-5.html
+// http://www.fractalforums.com/programming/smooth-colouring-of-convergent-fractals/
