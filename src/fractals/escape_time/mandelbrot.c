336472613f3847b05a13c4e43b65c21dc5433419
hakolao <okkohakola@gmail.com>
2020-02-21 00:08:56 +0200

    Add small optimization

--- src/fractals/escape_time/mandelbrot.c
+++ src/fractals/escape_time/mandelbrot.c
@@ -6,11 +6,22 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/19 15:34:24 by ohakola           #+#    #+#             */
-/*   Updated: 2020/02/20 15:48:02 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/02/20 23:57:49 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+static int			out_circle(long double center_x,
+					long double center_y, long double r,
+					long double x, long double y)
+{
+	x -= center_x;
+	y -= center_y;
+	if (x * x + y * y > r * r)
+		return(TRUE);
+	return(FALSE);
+}
 
 /*
 ** https://en.wikipedia.org/wiki/Plotting_
@@ -29,6 +40,12 @@ long double			mandelbrot_escape(t_complex z, t_complex c,
 	long double				iter;
 
 	iter = 0.0;
+	if (!(out_circle(-0.11, 0.0, 0.63, c.r, c.i) || c.r > 0.1)
+		&& out_circle(-1.0, 0.0, 0.25, c.r, c.i)
+		&& out_circle(-0.125, 0.744, 0.092, c.r, c.i)
+		&& out_circle(-1.308, 0.0, 0.058, c.r, c.i)
+		&& out_circle(0.0, 0.25, 0.35, c.r, c.i))
+		return (iter);
 	while (squares->r + squares->i <= 16 && iter < params->max_iter)
 	{
 		z.i = 2 * (z.i * z.r) + c.i;
