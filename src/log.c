b421b923cdf6bc32bd6b09f647f46b3e86bd6b55
Okko Hakola <ohakola@c2r6p4.hive.fi>
2020-02-23 16:28:29 +0200

    Clarify terminal guide

--- src/log.c
+++ src/log.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/11 12:21:55 by ohakola           #+#    #+#             */
-/*   Updated: 2020/02/23 16:15:10 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/02/23 16:27:38 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,16 @@ int				log_perr(char *str)
 int				log_guide(void)
 {
 	ft_putstr("-----------\nFRACTOL\n"
-			"-----------\n"
-			"Press key G for guide on screen.\n"
-			"Press Esc to exit\n\n"
-			"Allowed arguments:\n\nall\n"
-			"mandelbrot (Default)\njulia"
-			"\nburning_ship\nmandelbrot_n\njulia_n\nbird_of_prey"
-			"\njulia_mod\nphoenix_mod\nburning_julia\n"
-			"-colors=255,22,22\\|20,20,33\n\n"
-			"Colors: RGBs (as many as you'd like) split by '|'"
-			" and numbers between "
-			"0 and 255. Backslash must be escaped\n"
-			"Example: ./fractol mandelbrot julia "
-			"-colors=255,0,0\\|0,255,0\\|0,0,255\n"
-			"----------------------------------------\n");
+		"-----------\n"
+		"usage:\n ./fractol [fractal_name] [-colors=[r,g,b\\|r,g,b\\|+]\n\n"
+		"fractals:\n mandelbrot (Default)\n julia"
+		"\n burning_ship\n mandelbrot_n\n julia_n\n bird_of_prey"
+		"\n julia_mod\n phoenix_mod\n burning_julia\n\n"
+		"example:\n ./fractol mandelbrot julia "
+		"-colors=255,0,0\\|0,255,0\\|0,0,255\n\n"
+		"more options:\n"
+		" ESC to exit\n"
+		" G for guide & further actions on screen.\n"
+		"----------------------------------------\n");
 	return (1);
 }
