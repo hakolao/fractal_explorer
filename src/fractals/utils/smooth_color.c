e2899f87eabf3802a8c9afc2f0f14ff3d24e8e2e
Okko Hakola <ohakola@c2r6p4.hive.fi>
2020-02-24 17:36:47 +0200

    Don't allow iter going negative

--- src/fractals/utils/smooth_color.c
+++ src/fractals/utils/smooth_color.c
@@ -6,7 +6,7 @@
 /*   By: ohakola <ohakola@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/20 19:50:45 by ohakola           #+#    #+#             */
-/*   Updated: 2020/02/20 21:18:58 by ohakola          ###   ########.fr       */
+/*   Updated: 2020/02/24 17:31:19 by ohakola          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void				smooth_color_pixel(t_pixel *pixel, long double iter,
 					t_complex squares, t_fractal_params *params)
 {
 	iter = iter + 1.0 - log(log(squares.r + squares.i)) / log(params->pow_n);
+	if (iter < 0.0)
+		iter = 0;
 	pixel->color = lerp_rgb(params->palette[
 			(int)floor(iter) % params->palette_size],
 		params->palette[
